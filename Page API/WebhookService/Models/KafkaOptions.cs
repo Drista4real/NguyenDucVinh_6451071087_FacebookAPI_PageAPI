1aa17f4a11a9537cc3dc389b2a877f78bf1426f0
kats205 <npbaokhanhpro@gmail.com>
2026-05-16 10:19:12 +0700

    refactor: restructure projects into 4 independent service repositories

--- Page API/WebhookService/Models/KafkaOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace WebhookService.Models;
-
-public class KafkaOptions
-{
-    public string BootstrapServers { get; set; } = string.Empty;
-    public string Topic { get; set; } = string.Empty;
-}
--- Page API/WebhookService/Services/IKafkaEventPublisher.cs
+++
@@ -1,8 +0,0 @@
-using WebhookService.Models;
-
-namespace WebhookService.Services;
-
-public interface IKafkaEventPublisher
-{
-    Task PublishAsync(IReadOnlyList<NormalizedFacebookEvent> events, CancellationToken cancellationToken = default);
-}
